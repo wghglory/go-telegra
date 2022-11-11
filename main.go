10e86cc6d5c8ee47abfe5c328f03784bd4476c73
Derek Wang <guanghuiw@vmware.com>
2022-11-11 16:40:59 +0800

    feat: getPage API

--- main.go
+++ main.go
@@ -79,6 +79,43 @@ func main() {
 		return pageHandler(c, accessToken, path, title, content)
 	})
 
+	// http://localhost:8080/getPage/Sample-Page-12-15?return_content=true
+	app.Get("/getPage/:path", func(c *fiber.Ctx) error {
+		path := c.Params("path")
+		title := "New Title"
+		content := "[{\"tag\":\"div\",\"children\":[\"Hello, New world!\"]}]"
+
+		page := &model.Page{
+			Path:        path,
+			Title:       title,
+			AuthorName:  c.Query("author_name"),
+			Url:         fmt.Sprintf("http://localhost:8080/%v", path),
+			Description: c.Query("description"),
+			Content:     nil,
+			Views:       1,
+			CanEdit:     true,
+		}
+
+		var nodes []model.NodeElement
+		err := json.Unmarshal([]byte(content), &nodes)
+
+		if err != nil {
+			return c.JSON(&model.Response{
+				Ok:    false,
+				Error: "Fail to provide a valid DOM element",
+			})
+		}
+
+		if c.Query("return_content") == "true" {
+			page.Content = nodes
+		}
+
+		return c.JSON(&model.Response{
+			Ok:     true,
+			Result: page,
+		})
+	})
+
 	// http://localhost:8080/editPage/Sample-Page-12-15?access_token=d3b25feccb89e508a9114afb82aa421fe2a9712b963b387cc5ad71e58722&title=Sample+Page&author_name=Derek+Wang&content=[%7B%22tag%22:%22div%22,%22children%22:[%22Hello,+New+world!%22]%7D]&return_content=true
 	// http://localhost:8080/editPage/Sample-Page-12-15?access_token=d3b25feccb89e508a9114afb82aa421fe2a9712b963b387cc5ad71e58722&title=Sample+Page&author_name=Derek+Wang&content=[%7B%22tag%22:%22div%22,%22children%22:[%22Hello,+New+world!%22]%7D]&return_content=false
 	app.Get("/editPage/:path", func(c *fiber.Ctx) error {
